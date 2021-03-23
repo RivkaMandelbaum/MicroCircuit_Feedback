713883107c19fc0a256c203bb3240202447c4321
Rivka Mandelbaum <rivkamandelbaum@rivkas-mbp.lan>
2021-03-23 18:21:31 -0400

    Updated calculate firing rates tool and some input/output related files

--- calculateFiringRates.py
+++ calculateFiringRates.py
@@ -10,22 +10,28 @@ def main():
     """
     # get input
     args = sys.argv[1:]
-    if len(args) < 2: 
+    if len(args) < 2 or len(args) > 4: 
         print "usage: stimulus_file outputs_file [neuron] [--layer4]"
         sys.exit(1)
-    
+     
     stim_filename = args[0]
     output_filename = args[1]
 
     is_layer_four = False
     one_stim_only = False
 
-    if len(args) == 3:
+    if len(args) >= 3:
         if args[2] == "--layer4":
-            is_layer_four = True
-        elif type(args[2]) is int:
+             is_layer_four = True
+             if len(args) == 4:
+                 one_stim_only = True
+                 stimulus = int(args[3])
+        else:
             one_stim_only = True
-            stimulus = args[2]
+            stimulus = int(args[2])
+            if len(args) == 4:
+                is_layer_four = True
+        
 
     # open files
     stim_file = open(stim_filename, 'r')
@@ -48,27 +54,43 @@ def main():
     num_neurons = len(out_line)
 
     indexed_by_stim = [[0 for j in range(num_neurons)] for i in range(num_stimuli)]
+    
+    num_timebins_stimulus = [0 for i in range(num_stimuli)]
 
     # main loop
-    while(stim_line != ""):
+    while(stim_line and out_line[0] != ""):
+
+        # turn the strings into lists (can't do this before checking if they're empty)
+        stim_line = [int(i) for i in stim_line]
+        out_line = [int(i) for i in out_line]
+
         # calculate which stimulus it is
         curr_stim = find_stim(stim_line)
+
+        num_timebins_stimulus[curr_stim] += 1
 
         if one_stim_only:
             if curr_stim == stimulus:
                 index = 0
+            else: 
+                index = -1
         else:
             index = curr_stim
 
 
         # add 1 to the firing rate of each neuron that fired
-        indexed_by_stim[index]+= out_line
+        if index != -1:
+            indexed_by_stim[index] = [indexed_by_stim[index][i] + out_line[i] for i in range(num_neurons)]
 
         # update lines
-        stim_line = [int(i) for i in stim_file.readline().split()]
-        out_line = [int(i) for i in output_file.readline().split(", ")]
+        stim_line = stim_file.readline().split()
+        out_line = output_file.readline().split(", ")
 
-    print indexed_by_stim
+    for i in range(num_stimuli):
+        num_stim_appeared = float(num_timebins_stimulus[i])
+        print >> sys.stderr, num_stim_appeared
+        print "Stimulus %d," % i,
+        print ", ".join(str(j/num_stim_appeared) for j in indexed_by_stim[i])
     
 
 
